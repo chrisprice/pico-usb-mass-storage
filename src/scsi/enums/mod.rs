e7f96badb41feb55add95a4e2895ab52a1c77976
Rob Pilling <robpilling@gmail.com>
2024-05-31 16:26:13 +0100

    Comment-out unused enums (until supported by `#[overlay]`)

--- src/scsi/enums/mod.rs
+++ src/scsi/enums/mod.rs
@@ -17,19 +17,19 @@ mod page_control;
 pub use page_control::*;
 
 mod peripheral_qualifier;
-pub use peripheral_qualifier::*;
+//pub use peripheral_qualifier::*;
 
 mod peripheral_device_type;
-pub use peripheral_device_type::*;
+//pub use peripheral_device_type::*;
 
 mod version_descriptor;
 pub use version_descriptor::*;
 
 mod target_port_group_support;
-pub use target_port_group_support::*;
+//pub use target_port_group_support::*;
 
 mod spc_version;
 pub use spc_version::*;
 
 mod response_data_format;
-pub use response_data_format::*;
+//pub use response_data_format::*;
