6eb72d87dc72e5710fe4317c57fc848f0d69f9f6
Simon Martin <smartin@scottlogic.com>
2024-06-03 13:52:04 +0100

    Use predefined partition and filesystem

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
 pub mod bulk_only_transport;
+pub mod fat12_partition;
 pub mod scsi;
 pub mod usb_mass_storage;
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,9 @@ use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_usb::{Builder, Config};
 use embedded_io_async::ReadExactError;
 use panic_probe as _;
-use pico_usb_mass_storage::{bulk_only_transport::CommandError, usb_mass_storage::UsbMassStorage};
+use pico_usb_mass_storage::{
+    bulk_only_transport::CommandError, fat12_partition, usb_mass_storage::UsbMassStorage,
+};
 
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
@@ -30,8 +32,8 @@ static mut STATE: State = State {
     sense_qualifier: None,
 };
 
-const BLOCK_SIZE: u32 = 512;
-const BLOCKS: u32 = 200;
+pub const BLOCK_SIZE: u32 = 512;
+pub const BLOCKS: u32 = 200;
 const USB_PACKET_SIZE: u16 = 64; // 8,16,32,64
 const MAX_LUN: u8 = 0; // max 0x0F
 
@@ -50,6 +52,10 @@ impl State {
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    unsafe {
+        fat12_partition::init(&mut STORAGE, BLOCK_SIZE, BLOCKS);
+    }
+
     let p = embassy_rp::init(Default::default());
 
     let driver = Driver::new(p.USB, Irqs);
@@ -145,8 +151,13 @@ impl pico_usb_mass_storage::scsi::Handler for Handler {
                 })?;
         }
 
-        log_fs();
-        //log_storage();
+        unsafe {
+            fat12_partition::log_fs(&mut STORAGE, BLOCKS as u64, BLOCK_SIZE as u64);
+            for id in 0..4 {
+                let partition = fat12_partition::read_partition(&STORAGE, id);
+                info!("partition {}: {}", id, partition);
+            }
+        }
         Ok(())
     }
 
@@ -308,217 +319,5 @@ impl pico_usb_mass_storage::scsi::Handler for Handler {
             STATE.sense_qualifier.replace(0x00); // Invalid command operation ASCQ
         }
         Err(CommandError::CommandFailed)
-    }
-}
-
-#[derive(Clone)]
-pub struct Partition {
-    /// Partition Status
-    pub p_status: u8,
-    /// Start cylinder (Legacy CHS)
-    pub p_cyl_begin: u8,
-    /// Start head (Legacy CHS)
-    pub p_head_begin: u8,
-    /// Start sector (Legacy CHS)
-    pub p_sect_begin: u8,
-    /// Partition Type (DOS, Windows, BeOS, etc)
-    pub p_type: u8,
-    /// End cylinder (Legacy CHS)
-    pub p_cyl_end: u8,
-    /// End head (Legacy CHS)
-    pub p_head_end: u8,
-    /// End sector
-    pub p_sect_end: u8,
-    /// Logical block address to start of partition
-    pub p_lba: u32,
-    /// Number of sectors in partition
-    pub p_size: u32,
-}
-impl core::fmt::Debug for Partition {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct("Partition")
-            .field("p_type", &format_args!("{:#02x}", self.p_type))
-            .field("p_status", &format_args!("{:#02x}", self.p_status))
-            .field("p_cyl_begin", &format_args!("{:#02x}", self.p_cyl_begin))
-            .field("p_cyl_end", &format_args!("{:#02x}", self.p_cyl_end))
-            .field("p_head_begin", &format_args!("{:#02x}", self.p_head_begin))
-            .field("p_head_end", &format_args!("{:#02x}", self.p_head_end))
-            .field("p_sect_begin", &format_args!("{:#02x}", self.p_sect_begin))
-            .field("p_sect_end", &format_args!("{:#02x}", self.p_sect_end))
-            .field("p_lba", &format_args!("{:#08x}", self.p_lba))
-            .field("p_size", &format_args!("{:#08x}", self.p_size))
-            .finish()
-    }
-}
-pub struct ByteReader {
-    position: u64,
-}
-impl ByteReader {
-    fn new(position: u64) -> Self {
-        Self { position }
-    }
-    fn read1(&mut self) -> u8 {
-        let value = unsafe { STORAGE[self.position as usize] };
-        self.position += 1;
-        value
-    }
-    fn read4(&mut self) -> u32 {
-        let value = unsafe {
-            ((STORAGE[(self.position + 3) as usize] as u32) << 24)
-                + ((STORAGE[(self.position + 2) as usize] as u32) << 16)
-                + ((STORAGE[(self.position + 1) as usize] as u32) << 8)
-                + ((STORAGE[(self.position + 0) as usize] as u32) << 0)
-        };
-        self.position += 4;
-        value
-    }
-}
-fn read_partition(index: u8) -> Partition {
-    defmt::assert!(index < 4);
-
-    let position: u64 = 446 + (16 * (index as u64));
-
-    let mut byte_reader = ByteReader::new(position);
-
-    Partition {
-        p_status: byte_reader.read1(),
-        p_head_begin: byte_reader.read1(),
-        p_sect_begin: byte_reader.read1(),
-        p_cyl_begin: byte_reader.read1(),
-        p_type: byte_reader.read1(),
-        p_head_end: byte_reader.read1(),
-        p_sect_end: byte_reader.read1(),
-        p_cyl_end: byte_reader.read1(),
-        p_lba: byte_reader.read4(),
-        p_size: byte_reader.read4(),
-    }
-}
-
-fn log_fs() {
-    let options = fatfs::FsOptions::new().update_accessed_date(false);
-
-    let disk = MemFS::new();
-    let fs = match fatfs::FileSystem::new(disk, options) {
-        Ok(fatfs) => fatfs,
-        Err(e) => {
-            let s = match e {
-                fatfs::Error::Io(e) => "unknown: MemFSError",
-                fatfs::Error::UnexpectedEof => "UnexpectedEof",
-                fatfs::Error::WriteZero => "WriteZero",
-                fatfs::Error::InvalidInput => "InvalidInput",
-                fatfs::Error::NotFound => "NotFound",
-                fatfs::Error::AlreadyExists => "AlreadyExists",
-                fatfs::Error::DirectoryIsNotEmpty => "DirectoryIsNotEmpty",
-                fatfs::Error::CorruptedFileSystem => "CorruptedFileSystem",
-                fatfs::Error::NotEnoughSpace => "NotEnoughSpace",
-                fatfs::Error::InvalidFileNameLength => "InvalidFileNameLength",
-                fatfs::Error::UnsupportedFileNameCharacter => "UnsupportedFileNameCharacter",
-                _ => "Unknown",
-            };
-            error!("Error: {}", s);
-            return;
-        }
-    };
-    let fat_type = match fs.fat_type() {
-        fatfs::FatType::Fat12 => "Fat12",
-        fatfs::FatType::Fat16 => "Fat16",
-        fatfs::FatType::Fat32 => "Fat32",
-    };
-    let volume_id = fs.volume_id();
-    let volume_label: &str = core::str::from_utf8(fs.volume_label_as_bytes()).unwrap();
-
-    info!(
-        "type = {}, id = {}, label = {}",
-        fat_type, volume_id, volume_label
-    );
-    let root = fs.root_dir();
-    for d in root.iter().flatten() {
-        let filename = core::str::from_utf8(d.short_file_name_as_bytes()).unwrap();
-        let size = d.len();
-        info!(
-            "file name = \"{}\", size = {}, is_file: {}, is_dir: {}",
-            filename,
-            size,
-            d.is_file(),
-            d.is_dir()
-        );
-    }
-}
-
-struct MemFS {
-    offset: u64,
-}
-impl MemFS {
-    fn new() -> Self {
-        Self { offset: 0 }
-    }
-}
-impl fatfs::IoBase for MemFS {
-    type Error = MemFSError;
-}
-impl fatfs::Read for MemFS {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
-        let limit = usize::min(
-            buf.len(),
-            ((BLOCK_SIZE * BLOCKS) as u64 - self.offset)
-                .try_into()
-                .unwrap(),
-        );
-
-        unsafe {
-            let src = STORAGE[self.offset as usize..self.offset as usize + limit].as_ptr();
-            let dst = buf.as_mut_ptr();
-            core::ptr::copy(src, dst, limit)
-        };
-        self.offset += limit as u64;
-        Ok(limit)
-    }
-}
-impl fatfs::Write for MemFS {
-    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-        let limit = usize::min(
-            buf.len(),
-            ((BLOCK_SIZE * BLOCKS) as u64 - self.offset)
-                .try_into()
-                .unwrap(),
-        );
-
-        unsafe {
-            //let mut buf = buf.as_ptr();
-            let dst = STORAGE[self.offset as usize..self.offset as usize + limit].as_mut_ptr();
-            core::ptr::copy(buf.as_ptr(), dst, limit)
-        };
-        self.offset += limit as u64;
-        Ok(limit)
-    }
-
-    fn flush(&mut self) -> Result<(), Self::Error> {
-        unimplemented!()
-    }
-}
-impl fatfs::Seek for MemFS {
-    fn seek(&mut self, pos: fatfs::SeekFrom) -> Result<u64, Self::Error> {
-        match pos {
-            fatfs::SeekFrom::Start(p) => self.offset = p,
-            fatfs::SeekFrom::End(p) => self.offset = ((BLOCKS * BLOCK_SIZE) as i64 - p) as u64,
-            fatfs::SeekFrom::Current(p) => self.offset = (self.offset as i64 + p) as u64,
-        }
-        Ok(self.offset)
-    }
-}
-
-#[derive(Debug)]
-struct MemFSError {}
-impl fatfs::IoError for MemFSError {
-    fn is_interrupted(&self) -> bool {
-        false
-    }
-
-    fn new_unexpected_eof_error() -> Self {
-        unimplemented!()
-    }
-
-    fn new_write_zero_error() -> Self {
-        unimplemented!()
     }
 }
