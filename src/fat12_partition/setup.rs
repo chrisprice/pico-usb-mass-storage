aac04297f4a9f143d7231bb228945ff024bcf74a
Rob Pilling <robpilling@gmail.com>
2024-06-24 22:54:15 +0100

    Partial fat12 handling of STORAGE / `[Block]`

--- src/fat12_partition/setup.rs
+++ src/fat12_partition/setup.rs
@@ -1,6 +1,13 @@
+use crate::Block;
+
 static FS_DUMP: &[u8; 102400] = include_bytes!("../../dumps/linux_partitioned.dump");
-pub fn init(storage: &mut [u8], block_size: u32, blocks: u32) {
-    assert!(storage.len() >= (blocks * block_size) as usize);
-    assert!(storage.len() >= FS_DUMP.len());
-    storage[..FS_DUMP.len()].copy_from_slice(FS_DUMP);
+
+pub fn init(storage: &mut [Block]) {
+    assert!(storage.len() * crate::BLOCK_SIZE >= FS_DUMP.len());
+
+    // FIXME: need to update the slice length
+    let raw = storage as *mut _ as *mut [u8];
+    let raw = unsafe { &mut *raw };
+
+    raw[..FS_DUMP.len()].copy_from_slice(FS_DUMP);
 }
